e1c113d67dc2ddb1cab8c6e8172ae319543c1fae
Darcey Iwashita <darcey@gmail.com>
2017-01-31 08:37:36 -1000

    callback for animation added

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,27 +1,5 @@
-// Write a function named forEach in the utilities.js file. 
-
-// The goal is to write your own function named forEach. It should:
-// 		Use a loop to go through all elements in the points array.
-// 		Execute a callback for each element.
-
-// Replace the for loop in the animatePoints function with a forEach 
-// block and confirm that the selling points still animate properly.
-
-// non callback way:
-var forEach = function(points) {
- 	for(var i = 0; i < points.length; i++) {
- 		return i;
- 	}
-};
-
-var getItemsInPoints = function(points, forEach) {
-  return forEach(points);
-};
-
-
-
-// function forEach(points, callback) {
-//  	for(var i = 0; i < points.length; i++) {
-//  		callback(i);
-//  	}
-// }
+function forEach(array, callback) {
+     for(var i = 0; i < array.length; i++) {
+         callback(array[i]);
+     }
+}
