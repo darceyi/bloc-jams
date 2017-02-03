74d408b2fb87a81fe54e337171b087febe486a6b
Darcey Iwashita <darcey@gmail.com>
2017-02-02 21:20:29 -1000

    changed mouseover and mouseleave behavior and play to pause button

--- scripts/album.js
+++ scripts/album.js
@@ -80,6 +80,10 @@ var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var urrentlyPlayingSong = null;
+
 window.onload = function() {
 	setCurrentAlbum(albumPicasso);
 
@@ -92,17 +96,61 @@ window.onload = function() {
 		}
 	};
 
+	var getSongItem = function(element){
+  		switch (element.className){
+   			case "song-item-number":
+      			return element;
+      		case 'album-song-button':
+	        case 'ion-play':
+	        case 'ion-pause':
+           		return findParentByClassName(element, 'song-item-number');
+	        case 'album-view-song-item':
+	            return element.querySelector('.song-item-number');
+	        case 'song-item-title':
+	        case 'song-item-duration':
+	            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+	        default:
+	            return;
+      	}
+    };
+
+    var clikHandler = function(targetElement){
+      	var songItem = getSongItem(targetElement);
+      	if(currentlyPlayingSong === null) {
+        	songItem.innerHTML = pauseButtonTemplate;
+        	currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     	} else if(currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+        	songItem.innerHTML = playButtonTemplate;
+        	currentlyPlayingSong = null;
+        } else if(currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+        	var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+        	currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+        	songItem.innerHTML = pauseButtonTemplate;
+        	currentlyPlayingSong = songItem.getAttribute('data-song-number');
+   		}
+    };
+
 	songListContainer.addEventListener('mouseover', function(event) {
-		if(event.target.parentElement.className === 'album-view-song-item'){
-			event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-		}
+		if(event.target.parentElement.className === 'album-view-song-item') {
+			var songItem = getSongItem(event.target);
++           if(songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
++                songItem.innerHTML = playButtonTemplate;
++           }
+		} 
 	});
 
 	for(var i = 0; i < songRows.length; i++) {
 		songRows[i].addEventListener('mouseleave', function(event) {
-			this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+			var songItem = getSongItem(event.target);
+			var songItemNumber = songItem.getAttribute('data-song-number');
+			if(songItemNumber !== currentlyPlayingSong) {
+				songItem.innerHTML = songItemNumber;
+			}
 		});
-	}
+
+		songRows[i].addEventListener('click', function(event) {
+			clickHandler(event.target);
+		});
 
 	var albums = [albumPicasso, albumDerp, albumMarconi];
 
@@ -117,5 +165,4 @@ window.onload = function() {
 			index = 0;
 		}
 	});
-};
-
+}
