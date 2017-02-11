42e330d016b1b517e72b4dc19282effd45761a95
Darcey Iwashita <darcey@gmail.com>
2017-02-10 22:20:42 -1000

    began checkpoint 18 refactoring with .hover()

--- scripts/album.js
+++ scripts/album.js
@@ -52,7 +52,15 @@ var createSongRow = function(songNumber, songName, songLength) {
 	+	'</tr>'
 	;
 
-	return $(template);
+	var $row = $(template);
+
+	//find the element with the .song-item-number class that's contained in whichever row is clicked
+	$row.find('.song-item-number').click(clickHandler);
+	//The hover() event listener at #2 combines the mouseover and mouseleave functions. 
+	//The first argument is a callback that executes when the user mouses over the $row element 
+	//and the second is a callback executed when the mouse leaves $row.
+	$row.hover(onHover, offHover);
+	return $row;
 };
 
 var setCurrentAlbum = function(album) {
@@ -84,8 +92,8 @@ var setCurrentAlbum = function(album) {
 };
 
 // Elements we'll be adding listeners to
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
+var $songListContainer = $('.album-view-song-list');
+var $songRows = $('.album-view-song-item');
 
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
@@ -94,7 +102,7 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
 // Store state of playing songs
 var currentlyPlayingSong = null;
 
-window.onload = function() {
+$(document).ready(function() {
 	setCurrentAlbum(albumPicasso);
 
 	var findParentByClassName = function(element, targetClass) {
@@ -110,24 +118,6 @@ window.onload = function() {
 			console.log('No parent found with that class name');
 		}
 	};
-
-	var getSongItem = function(element){
-  		switch (element.className){
-   			case 'song-item-number':
-      			return element;
-      		case 'album-song-button':
-	        case 'ion-play':
-	        case 'ion-pause':
-           		return findParentByClassName(element, 'song-item-number');
-	        case 'album-view-song-item':
-	            return element.querySelector('.song-item-number');
-	        case 'song-item-title':
-	        case 'song-item-duration':
-	            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-	        default:
-	            return;
-      	}
-    };
 
     var clickHandler = function(targetElement){
       	var songItem = getSongItem(targetElement);
@@ -145,28 +135,35 @@ window.onload = function() {
    		}
     };
 
-	songListContainer.addEventListener('mouseover', function(event) {
+//Attempt to write the onHover and offHover functions. hover()
+//Note that we no longer need to use the getSongItem() helper because we can use jQuery's find() method to 
+//get the element with .song-item-number. Use this to refer to the row.
+	var onHover = function() {
+		$songListContainer.addEventListener('mouseover', function(event) {
 		if (event.target.parentElement.className === 'album-view-song-item') {
 			var songItem = getSongItem(event.target);
 			if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
                 songItem.innerHTML = playButtonTemplate;
           	}
 		} 
-	});
+	};
 
-	for (var i = 0; i < songRows.length; i++) {
-		songRows[i].addEventListener('mouseleave', function(event) {
-			var songItem = getSongItem(event.target);
-			var songItemNumber = songItem.getAttribute('data-song-number');
-			if (songItemNumber !== currentlyPlayingSong) {
-				songItem.innerHTML = songItemNumber;
-			}
-		});
+	var offHover = function() {
 
-		songRows[i].addEventListener('click', function(event) {
-			clickHandler(event.target);
-		});
-	}
+		for (var i = 0; i < songRows.length; i++) {
+			songRows[i].addEventListener('mouseleave', function(event) {
+				var songItem = getSongItem(event.target);
+				var songItemNumber = songItem.getAttribute('data-song-number');
+				if (songItemNumber !== currentlyPlayingSong) {
+					songItem.innerHTML = songItemNumber;
+				}
+			});
+
+			songRows[i].addEventListener('click', function(event) {
+				clickHandler(event.target);
+			});
+		}
+	};
 
 	var albums = [albumPicasso, albumDerp, albumMarconi];
 
@@ -181,4 +178,4 @@ window.onload = function() {
 			index = 0;
 		}
 	});
-};
+});
